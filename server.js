9cf7385a9e425028da722a2c8e776deb45d80ee7
300699795 <karleisenyabut@hotmail.com>
2016-07-17 18:14:50 -0400

    test9

--- server.js
+++ server.js
@@ -3,18 +3,22 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('views', '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+////Deploying
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+////running locally
+//app.listen(3000);
+//console.log('running at 3000');
 
