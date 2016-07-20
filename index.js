c76ce978f056060d95819ec8a95ed5958af2beb9
300699795 <karleisenyabut@hotmail.com>
2016-07-20 18:01:13 -0400

    redo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-//app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -14,12 +14,12 @@ app.get('/', function(request, response) {
 });
 
 ////Deploying
-//app.listen(app.get('port'), function() {
-//  console.log('Node app is running on port', app.get('port'));
-//});
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 
 ////running locally
-app.listen(3000);
-console.log('running at 3000');
+//app.listen(3000);
+//console.log('running at 3000');
 
